56b03d22f00f032c9f470f16ad32fabc18dfee22
Adrian Haynes <majorx3music@gmail.com>
2026-04-05 16:29:38 -0400

    Add worker handler and set up prettier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { auth } from "./lib/auth";
 
 const app = new Hono();
-
+app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
 app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get("/", (c) => {
